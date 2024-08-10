94cf5ab7b59b4028912219cb17f9b940e802877a
Matheus Rangel <matheusrangel2003@hotmail.com>
2024-08-10 00:26:47 -0300

    update-7at16

---
+++ src/main/java/Exercícios_Algoritmo_JAVA/ex031.java
@@ -0,0 +1,49 @@
+//31) [DESAFIO] Crie um jogo de JoKenPo (Pedra-Papel-Tesoura)
+package Exercícios_Algoritmo_JAVA;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class ex031 {
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+        Random random = new Random();
+        int r = random.nextInt(1,3);
+        int escolha;
+        System.out.print("Escolha uma das opções (1)pedra,(2)papel,(3)tesoura: ");
+        escolha = teclado.nextInt();
+        if (r==1){
+            if (escolha==3){
+                System.out.print("Você perdeu ! o computador escolher pedra!");
+            }
+            if (escolha==2){
+                System.out.print("Você ganhou ! o computador escolher pedra!");
+            }
+            if (escolha==1){
+                System.out.print("Deu EMPATE!");
+            }
+        }
+        if (r==2){
+            if (escolha==1){
+                System.out.print("Você perdeu ! o computador escolher PAPEL!");
+            }
+            if (escolha==3){
+                System.out.print("Você ganhou ! o computador escolher PAPEL!");
+            }
+            if (escolha==2){
+                System.out.print("Deu EMPATE!");
+            }
+        }
+        if (r==3){
+            if (escolha==2){
+                System.out.print("Você perdeu ! o computador escolher PAPEL!");
+            }
+            if (escolha==1){
+                System.out.print("Você ganhou ! o computador escolher PAPEL!");
+            }
+            if (escolha==3){
+                System.out.print("Deu EMPATE!");
+            }
+        }
+    }
+}
---
+++ src/main/java/Exercícios_Algoritmo_JAVA/ex034.java
@@ -0,0 +1,36 @@
+package Exercícios_Algoritmo_JAVA;
+
+import java.util.Scanner;
+
+public class ex034 {
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+
+        // Declaração das variáveis
+        double altura, peso, imc;
+
+        // Entrada de dados
+        System.out.print("Digite sua altura (em metros): ");
+        altura = teclado.nextDouble();
+        System.out.print("Digite seu peso (em kg): ");
+        peso = teclado.nextDouble();
+
+        // Calcula o IMC
+        imc = peso / (altura * altura);
+
+        // Classificação do IMC
+        if (imc >= 40) {
+            System.out.println("OBESIDADE MÓRBIDA");
+        } else if (imc >= 30) {
+            System.out.println("OBESIDADE");
+        } else if (imc >= 25) {
+            System.out.println("SOBREPESO");
+        } else if (imc >= 18.5) {
+            System.out.println("PESO IDEAL");
+        } else {
+            System.out.println("ABAIXO DO PESO");
+        }
+
+        teclado.close();
+    }
+}
